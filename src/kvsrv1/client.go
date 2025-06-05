c6363d25abc70f17cdb4e6178541a56e45f9ac3c
mikeshannon2 <michael.shannon002@gmail.com>
2025-06-05 19:56:46 +0000

    Implemented client get and put methods

--- src/kvsrv1/client.go
+++ src/kvsrv1/client.go
@@ -2,10 +2,9 @@ package kvsrv
 
 import (
 	"6.5840/kvsrv1/rpc"
-	"6.5840/kvtest1"
-	"6.5840/tester1"
+	kvtest "6.5840/kvtest1"
+	tester "6.5840/tester1"
 )
-
 
 type Clerk struct {
 	clnt   *tester.Clnt
@@ -31,7 +30,10 @@ func MakeClerk(clnt *tester.Clnt, server string) kvtest.IKVClerk {
 func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
 
 	// You will have to modify this function.
-	return "", 0, rpc.ErrNoKey
+	args := rpc.GetArgs{Key: key}
+	reply := rpc.GetReply{}
+	ck.clnt.Call(ck.server, "KVServer.Get", &args, &reply)
+	return reply.Value, reply.Version, reply.Err
 }
 
 // Put updates key with value only if version is the version in the
@@ -53,5 +55,9 @@ func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) Put(key, value string, version rpc.Tversion) rpc.Err {
 	// You will have to modify this function.
-	return rpc.ErrNoKey
+
+	args := rpc.PutArgs{Key: key, Value: value, Version: version}
+	reply := rpc.PutReply{}
+	ck.clnt.Call(ck.server, "KVServer.Put", &args, &reply)
+	return reply.Err
 }
